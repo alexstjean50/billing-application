9131c5cf3c7e892d33dea5dd1787944f81399ae6
alexstjean50 <alexstjean50@gmail.com>
2017-12-20 14:51:49 -0500

    CRM and stuff

--- crm/src/main/java/ca/ulaval/glo4002/crm/CrmServer.java
+++ crm/src/main/java/ca/ulaval/glo4002/crm/CrmServer.java
@@ -1,10 +1,12 @@
 package ca.ulaval.glo4002.crm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationListener;
 
 public class CrmServer implements Runnable
 {
     private String[] args;
+    private ApplicationListener<?> listener;
 
     public static void main(String[] args) throws Exception
     {
@@ -16,9 +18,20 @@ public class CrmServer implements Runnable
         this.args = args;
     }
 
+    public CrmServer(String[] args, ApplicationListener<?> listener)
+    {
+        this.args = args;
+        this.listener = listener;
+    }
+
     @Override
     public void run()
     {
-        SpringApplication.run(CrmSpringApplication.class, args);
+        SpringApplication app = new SpringApplication(CrmSpringApplication.class);
+        if (listener != null)
+        {
+            app.addListeners(listener);
+        }
+        app.run(args);
     }
 }
