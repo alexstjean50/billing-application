3a6076edc128c69849084ecd118630a6e8373c52
alexstjean50 <alexstjean50@gmail.com>
2017-12-22 18:45:04 -0500

    Refactoring

--- billing/src/test/java/ca/ulaval/glo4002/crm/CrmIntegrationRestTest.java
+++ billing/src/test/java/ca/ulaval/glo4002/crm/CrmIntegrationRestTest.java
@@ -5,6 +5,8 @@ import ca.ulaval.glo4002.billing.persistence.entity.ProductEntity;
 import ca.ulaval.glo4002.commons.RestTestBase;
 import org.junit.Test;
 
+import javax.ws.rs.core.Response;
+
 import static org.junit.Assert.assertEquals;
 
 public class CrmIntegrationRestTest extends RestTestBase
@@ -12,6 +14,7 @@ public class CrmIntegrationRestTest extends RestTestBase
     private static final String CRM_API_URL = "http://localhost:8080";
     private static final int PRODUCT_ID = 1;
     private static final int CLIENT_ID = 1;
+    private static final int INVALID_ID = -1;
 
     @Test
     public void givenAClientId_whenRetrievingClient_thenShouldReturnCorrespondingClient()
@@ -31,5 +34,23 @@ public class CrmIntegrationRestTest extends RestTestBase
                 .as(ProductEntity.class);
 
         assertEquals(PRODUCT_ID, productEntity.getId());
+    }
+
+    @Test
+    public void givenAnInvalidClientId_whenRetrievingClient_thenShouldReturnNotFoundStatus()
+    {
+        givenBaseRequest().baseUri(CRM_API_URL)
+                .get("/clients/{id}", buildPathParams("id", INVALID_ID))
+                .then()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    public void givenAnInvalidProductId_whenRetrievingProduct_thenShouldReturnNotFoundStatus()
+    {
+        givenBaseRequest().baseUri(CRM_API_URL)
+                .get("/products/{id}", buildPathParams("id", INVALID_ID))
+                .then()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode());
     }
 }
